71b94627521ddb91e82fa2db3ac4c9bab0fb58d6
h8uthemost <h8uthemost@gmail.com>
2024-05-17 20:56:36 -0500

    Update rc.lua

--- rc.lua
+++ rc.lua
@@ -1,24 +1,3 @@
---  rc.lua
---  custom initialisation for awesome windowmanager 4.0.x
---
- -- Copyright (C) 2012, 2013 by Togan Muftuoglu <toganm@opensuse.org>
- -- Copyright (C) 2015, 2016 by Sorokin Alexei <sor.alexei@meowr.ru>
- -- This program is free software; you can redistribute it and/or
- -- modify it under the terms of the GNU General Public License as
- -- published by the Free Software Foundation; either version 2, or (at
- -- your option) any later version.
-
- -- This program is distributed in the hope that it will be useful, but
- -- WITHOUT ANY WARRANTY; without even the implied warranty of
- -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- -- General Public License for more details.
-
- -- You should have received a copy of the GNU General Public License
- -- along with GNU Emacs; see the file COPYING.  If not, write to the
- -- Free Software Foundation, Inc.,  51 Franklin Street, Fifth Floor,
- -- Boston, MA 02110-1301 USA
-
-
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
